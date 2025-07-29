b25393fa6666f5339cf5dfdc6eb5abfcbbe12582
Alexandru <alexandru.margarit@netadm.ro>
2025-07-29 12:39:51 +0300

    wip

--- app/testReceiver.py
+++ app/testReceiver.py
@@ -1,4 +1,6 @@
-from receiverProxy import run_sequence
+from app.receiverProxy import modify_packet, parse_hex_dump, send_hex_packet, send_tcp_packet, wait_for_tcp_synack, wait_for_udp_response
+
+
 iface = "eth0"
 src_mac = "bc:24:11:b6:23:f8"
 dst_mac = "80:30:49:e0:c4:0d"
@@ -35,6 +37,97 @@ packet3_hex = """
 34 6a 02 c6 02 c4 02 c6 de 77 00 00 00 00 60 02
 05 b4 2e cd 00 00 02 04 05 b4 00 00
 """
+
+def run_sequence(iface, src_mac, dst_mac, src_ip, dst_ip,
+                 udp_port, src_tcp_port, dst_tcp_port,
+                 packet1_hex, packet2_hex, packet3_hex, file_to_send):
+
+    # Send first UDP packet
+    print("Sending first UDP packet...")
+    send_hex_packet(packet1_hex, iface, src_mac, dst_mac, src_ip, dst_ip)
+
+    # Wait for UDP response
+    if not wait_for_udp_response(iface, src_ip, dst_ip, udp_port):
+        print("Did not receive UDP response, aborting.")
+        return
+
+    # Send second UDP packet
+    print("Sending second UDP packet...")
+    send_hex_packet(packet2_hex, iface, src_mac, dst_mac, src_ip, dst_ip)
+
+    # Wait for UDP response again
+    if not wait_for_udp_response(iface, src_ip, dst_ip, udp_port):
+        print("Did not receive UDP response after second packet, aborting.")
+        return
+
+    # Send third TCP packet (SYN)
+    print("Starting TCP handshake with SYN packet...")
+    raw_bytes = parse_hex_dump(packet3_hex)
+    tcp_syn_pkt = modify_packet(raw_bytes, src_mac, dst_mac, src_ip, dst_ip)
+
+    # Make sure TCP ports match passed arguments
+    tcp_syn_pkt[TCP].sport = src_tcp_port
+    tcp_syn_pkt[TCP].dport = dst_tcp_port
+    del tcp_syn_pkt[TCP].chksum
+    del tcp_syn_pkt[IP].chksum
+    tcp_syn_pkt = tcp_syn_pkt.__class__(raw(tcp_syn_pkt))
+
+    send_tcp_packet(tcp_syn_pkt, iface)
+
+    # Wait for SYN+ACK
+    synack_pkt = wait_for_tcp_synack(iface, src_ip, dst_ip, src_tcp_port, dst_tcp_port)
+    if not synack_pkt:
+        print("TCP handshake failed, aborting.")
+        return
+
+    # Send ACK to complete handshake
+    tcp_ack_pkt = Ether(src=src_mac, dst=dst_mac)/\
+                  IP(src=src_ip, dst=dst_ip)/\
+                  TCP(sport=src_tcp_port, dport=dst_tcp_port,
+                      flags='A',
+                      seq=synack_pkt[TCP].ack,
+                      ack=synack_pkt[TCP].seq + 1)
+    send_tcp_packet(tcp_ack_pkt, iface)
+
+    # Open file and send contents over TCP (raw)
+    print(f"Sending file {file_to_send} over TCP...")
+    with open(file_to_send, 'rb') as f:
+        data = f.read()
+
+    # Send data in batches of 1460 bytes (typical MSS)
+    MSS = 1460
+    seq = tcp_ack_pkt[TCP].seq
+    ack = tcp_ack_pkt[TCP].ack
+    for i in range(0, len(data), MSS):
+        chunk = data[i:i+MSS]
+        pkt = Ether(src=src_mac, dst=dst_mac)/\
+              IP(src=src_ip, dst=dst_ip)/\
+              TCP(sport=src_tcp_port, dport=dst_tcp_port,
+                  flags='PA',
+                  seq=seq,
+                  ack=ack)/\
+              Raw(load=chunk)
+        del pkt[TCP].chksum
+        del pkt[IP].chksum
+        pkt = pkt.__class__(raw(pkt))
+        send_tcp_packet(pkt, iface)
+        seq += len(chunk)
+
+    print("File sent.")
+
+    # Close TCP connection (FIN handshake)
+    fin_pkt = Ether(src=src_mac, dst=dst_mac)/\
+              IP(src=src_ip, dst=dst_ip)/\
+              TCP(sport=src_tcp_port, dport=dst_tcp_port,
+                  flags='FA',
+                  seq=seq,
+                  ack=ack)
+    del fin_pkt[TCP].chksum
+    del fin_pkt[IP].chksum
+    fin_pkt = fin_pkt.__class__(raw(fin_pkt))
+    send_tcp_packet(fin_pkt, iface)
+    print("Sent FIN to close connection.")
+
 # Call your existing function that runs the handshake + sends file
 run_sequence(iface, src_mac, dst_mac, src_ip, dst_ip,
              udp_port, src_tcp_port, dst_tcp_port,
